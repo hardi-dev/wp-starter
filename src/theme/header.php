7b2b6d8d6d2a67f04385dfa524b259e09baeca06
Hardiansa <hardiansa@maleo.social>
2021-07-23 22:02:12 +0700

    init

---
+++ src/theme/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="author" content="Luan Gjokaj, and WordPressify contributors" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<header id="header" class="container header">
+	<h1>Header</h1>
+</header>
+<?php edit_post_link( 'Edit', '<p class="edit-button">', '</p>' ); ?>
